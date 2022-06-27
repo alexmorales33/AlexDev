0d0a52885baff15dfa7880f76218001460de2d42
alexmorales33 <alexmoraleess4@gmail.com>
2022-06-26 21:51:01 -0300

    MO

--- content/skills/skills.js
+++ content/skills/skills.js
@@ -1,23 +1,21 @@
 export const skills = {
-  Web: [
-    { name: 'HTML', percent: 95 },
-    { name: 'CSS', percent: 95 },
+  Frontend: [
+    { name: 'HTML/CSS', percent: 90 },
     { name: 'Javascript', percent: 90 },
     { name: 'React', percent: 85 },
-    { name: 'Node Js', percent: 70 },
-    { name: 'MySql', percent: 70 },
+    { name: 'Redux', percent: 80 },
+    { name: 'Typescript', percent: 80 },
   ],
-  Others: [
-    { name: 'Redux', percent: 85 },
-    { name: 'Sass', percent: 80 },
-    { name: 'Styled-Components', percent: 80 },
-    { name: 'Jquery', percent: 70 },
+  Backend: [
+    { name: 'NodeJs', percent: 80 },
+    { name: 'Express', percent: 80 },
+    { name: 'MySql', percent: 80 },
   ],
-  Tools: [
+  Design: [
+    { name: 'Figma', percent: 90 },
     { name: 'Photoshop', percent: 90 },
     { name: 'Illustrator', percent: 90 },
-    { name: 'Premiere', percent: 85 },
-    { name: 'Concepts', percent: 80 },
-    { name: 'Adobe xD', percent: 75 },
+    { name: 'Premiere', percent: 80 },
+    { name: 'Adobe xD', percent: 80 },
   ],
 };
